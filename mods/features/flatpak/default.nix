57cc90c3333619819017051a8ae1b02d05de8df2
Edward Groves <EdwardGroves2007@proton.me>
2026-07-16 01:10:40 -0400

    flatpak nix

---
+++ mods/features/flatpak/default.nix
@@ -0,0 +1,22 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.nixosModules.brave = {
+    pkgs,
+    lib,
+    ...
+  }: {
+services.flatpak.enable = true;
+  services.flatpak.packages = [
+    "moe.launcher.an-anime-game-launcher"
+    "moe.launcher.the-honkers-railway-launcher"
+    "moe.launcher.sleepy-launcher"
+  ];
+  services.flatpak.update.auto = {
+  enable = true;
+  onCalendar = "weekly"; # Default value
+  };
+ };
+}
